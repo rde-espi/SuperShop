1a7618940ec02a86c3d25bde591af9113ce76176
Reinaldo Souza <reinaldo_7531@hotmail.com>
2023-05-25 15:59:17 +0100

    First Controller and Database creation

---
+++ SuperShop/SuperShop.Web/Data/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SuperShop.Web.Data.Entities;
+
+namespace SuperShop.Web.Data
+{
+    public class DataContext:DbContext
+    {
+        public DbSet<Product>Products { get; set; }
+        public DataContext(DbContextOptions<DataContext>options): base(options)
+        {
+
+        }
+    }
+}
